d3c15d6013a297d7ca4ec2811b9f27a54f5783ec
huynhluan491 <huynhluan491@gmail.com>
2023-05-01 16:53:01 +0700

    CouponManagement-ADD-UI: giao diện thanh filter danh sách location

---
+++ src/app/HumanResource/components/filter-bar/filter-bar.component.ts
@@ -0,0 +1,40 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { ListlinkService } from 'src/app/assets/services/listlink.service';
+
+@Component({
+    selector: 'app-filter-bar',
+    templateUrl: './filter-bar.component.html',
+    styleUrls: ['./filter-bar.component.scss'],
+})
+export class FilterBarComponent {
+    searchInput: string = '';
+
+    @Input() SearchValue!: string;
+    @Input() isSearchValue?: boolean;
+    @Input() searchQuery?: string;
+    @Output() onCheckSearchValue: EventEmitter<string> = new EventEmitter();
+    @Output() handleResetType: EventEmitter<void> = new EventEmitter();
+    constructor(private myService: ListlinkService) {}
+
+    ngOnInit(): void {}
+
+    handleEnterSearch = (input: any) => {
+        console.log(input);
+
+        this.myService.getSearchQuery(input.target.value);
+        this.onCheckSearchValue.emit(input);
+        this.myService.getListLink();
+    };
+
+    handleIconSearch = (input: any) => {
+        console.log(input);
+        this.myService.getSearchQuery(input);
+        this.onCheckSearchValue.emit(input);
+        this.myService.getListLink();
+    };
+
+    handleResetFilter = () => {
+        this.myService.onResetFilter();
+        this.handleResetType.emit();
+    };
+}
