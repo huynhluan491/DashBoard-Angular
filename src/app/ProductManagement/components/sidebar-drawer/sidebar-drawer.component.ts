94773647ef399c3615094c08768115a5a760a134
huynhluan491 <huynhluan491@gmail.com>
2023-05-01 16:50:54 +0700

    CouponManagement-ADD-UI: thêm giao diện danh sách vị trí

--- src/app/ProductManagement/components/sidebar-drawer/sidebar-drawer.component.ts
+++ src/app/ProductManagement/components/sidebar-drawer/sidebar-drawer.component.ts
@@ -8,57 +8,7 @@ import { items } from 'src/app/item';
     styleUrls: ['./sidebar-drawer.component.scss'],
 })
 export class SidebarDrawerComponent {
-    // public selected = 'Coupon';
-    // public expanded = true;
-    // public sidebarItems = this.items.parents;
-    // public sideItems = this.items.parents;
-    // public itemIndex: number = 0;
-
-    // public onSelect(ev: DrawerSelectEvent): void {
-    //     ev.preventDefault();
-    //     this.selected = ev.item.text;
-    //     if (ev.item.level !== 1) {
-    //         this.sidebarItems = this.sidebarItems.map((i) => ({ ...i, selected: false })); // Deselecting all items as otherwise we end up with the wrong item selected.
-    //         ev.preventDefault();
-    //     }
-    //     const item = this.sidebarItems.find((e: any, index: any) => {
-    //         this.itemIndex = index;
-    //         return e.text === ev.item.text;
-    //     });
-
-    //     if (ev.item.text !== this.selected) {
-    //         item?.expanded ? (item.expanded = false) : (item!.expanded = true);
-    //         console.log(item?.selected);
-
-    //         item!.selected = !item?.selected;
-    //     }
-
-    //     if (!ev.item.children) {
-    //         this.selected = ev.item.text;
-    //     }
-
-    //     console.log(item);
-
-    //     if (ev.item.text === 'CHÍNH SÁCH') {
-    //         item?.expanded ? this.addChildren(this.items.couponPolicy) : this.removeChildren(this.items.couponPolicy);
-    //     }
-    //     console.log(this.items);
-    // }
-
-    // public switchExpanded(drawer: any): void {
-    //     this.expanded = !this.expanded;
-    //     drawer.toggle();
-    // }
-
-    // public addChildren(children: any) {
-    //     this.sidebarItems.splice(this.itemIndex + 1, 0, ...children);
-    // }
-
-    // public removeChildren(children: Array<any>) {
-    //     this.sidebarItems.splice(this.itemIndex + 1, children.length);
-    // }
-
-    public selected = 'Inbox';
+    public selected = 'Điểm làm việc';
     public expanded = true;
     public expandedIndices = [2];
 
--- src/app/test-lisst/test-lisst.component.ts
+++ src/app/test-lisst/test-lisst.component.ts
@@ -1,27 +1,226 @@
-import { Component } from '@angular/core';
-import { Router } from '@angular/router';
-import { ProductService } from '../product.service';
+import { Component, Input } from '@angular/core';
+
+const treeListData = [
+    {
+        ParentCode: null,
+        ProvinceCode: null,
+        DistrictCode: null,
+        WardCode: null,
+        StatusName: 'Duyệt áp dụng',
+        ListChild: [
+            {
+                ParentCode: null,
+                ProvinceCode: null,
+                DistrictCode: null,
+                WardCode: null,
+                StatusName: 'Duyệt áp dụng',
+                ListChild: [
+                    {
+                        ParentCode: null,
+                        ProvinceCode: null,
+                        DistrictCode: null,
+                        WardCode: null,
+                        StatusName: 'Duyệt áp dụng',
+                        ListChild: null,
+                        Code: 3,
+                        ParentID: 2,
+                        LocationID: 'DP42',
+                        LocationName: 'Kho 42',
+                        Brieft: null,
+                        Address: null,
+                        Province: null,
+                        District: null,
+                        Ward: null,
+                        Remark: null,
+                        StatusID: 2,
+                    },
+                ],
+                Code: 2,
+                ParentID: 1,
+                LocationID: 'LO42',
+                LocationName: 'Văn phòng 42',
+                Brieft: null,
+                Address: null,
+                Province: null,
+                District: null,
+                Ward: null,
+                Remark: null,
+                StatusID: 2,
+            },
+        ],
+        Code: 1,
+        ParentID: null,
+        LocationID: 'HW40',
+        LocationName: 'Văn phòng 40',
+        Brieft: null,
+        Address: null,
+        Province: null,
+        District: null,
+        Ward: null,
+        Remark: null,
+        StatusID: 2,
+    },
+    {
+        ParentCode: null,
+        ProvinceCode: null,
+        DistrictCode: null,
+        WardCode: null,
+        StatusName: 'Duyệt áp dụng',
+        ListChild: null,
+        Code: 4,
+        ParentID: null,
+        LocationID: 'OW40',
+        LocationName: 'Kho Online',
+        Brieft: null,
+        Address: null,
+        Province: null,
+        District: null,
+        Ward: null,
+        Remark: null,
+        StatusID: 2,
+    },
+    {
+        ParentCode: null,
+        ProvinceCode: null,
+        DistrictCode: null,
+        WardCode: null,
+        StatusName: 'Duyệt áp dụng',
+        ListChild: null,
+        Code: 5,
+        ParentID: null,
+        LocationID: 'HC01',
+        LocationName: 'Hachi Hachi NVT',
+        Brieft: null,
+        Address: null,
+        Province: null,
+        District: null,
+        Ward: null,
+        Remark: null,
+        StatusID: 2,
+    },
+    {
+        ParentCode: null,
+        ProvinceCode: null,
+        DistrictCode: null,
+        WardCode: null,
+        StatusName: 'Duyệt áp dụng',
+        ListChild: null,
+        Code: 6,
+        ParentID: null,
+        LocationID: 'HC02',
+        LocationName: 'Hachi Hachi PMH',
+        Brieft: null,
+        Address: null,
+        Province: null,
+        District: null,
+        Ward: null,
+        Remark: null,
+        StatusID: 2,
+    },
+    {
+        ParentCode: null,
+        ProvinceCode: null,
+        DistrictCode: null,
+        WardCode: null,
+        StatusName: 'Duyệt áp dụng',
+        ListChild: null,
+        Code: 7,
+        ParentID: null,
+        LocationID: 'HC03',
+        LocationName: 'Hachi Hachi CMT8',
+        Brieft: null,
+        Address: null,
+        Province: null,
+        District: null,
+        Ward: null,
+        Remark: null,
+        StatusID: 2,
+    },
+    {
+        ParentCode: null,
+        ProvinceCode: null,
+        DistrictCode: null,
+        WardCode: null,
+        StatusName: 'Duyệt áp dụng',
+        ListChild: null,
+        Code: 8,
+        ParentID: null,
+        LocationID: 'HC04',
+        LocationName: 'Hachi Hachi PT',
+        Brieft: null,
+        Address: null,
+        Province: null,
+        District: null,
+        Ward: null,
+        Remark: null,
+        StatusID: 2,
+    },
+    {
+        ParentCode: null,
+        ProvinceCode: null,
+        DistrictCode: null,
+        WardCode: null,
+        StatusName: 'Duyệt áp dụng',
+        ListChild: null,
+        Code: 9,
+        ParentID: null,
+        LocationID: 'HC05',
+        LocationName: 'Hachi Hachi QT',
+        Brieft: null,
+        Address: null,
+        Province: null,
+        District: null,
+        Ward: null,
+        Remark: null,
+        StatusID: 2,
+    },
+    {
+        ParentCode: null,
+        ProvinceCode: null,
+        DistrictCode: null,
+        WardCode: null,
+        StatusName: 'Duyệt áp dụng',
+        ListChild: null,
+        Code: 10,
+        ParentID: null,
+        LocationID: 'HC06',
+        LocationName: 'Hachi Hachi DXH',
+        Brieft: null,
+        Address: null,
+        Province: null,
+        District: null,
+        Ward: null,
+        Remark: null,
+        StatusID: 2,
+    },
+];
+
 @Component({
     selector: 'app-test-lisst',
     templateUrl: './test-lisst.component.html',
     styleUrls: ['./test-lisst.component.scss'],
 })
 export class TestLisstComponent {
-    newsData: any[] = [];
-    listProduct: any[] = [];
-    constructor(private productService: ProductService, private router: Router) {}
+    public treeNodes: any[] = treeListData;
+
+    @Input() drawerView: any;
+
+    constructor() {}
     ngOnInit(): void {
-        // this.productService.getAllNews().subscribe((data) => {
-        //     this.newsData = data;
-        //     console.log(this.newsData);
-        // });
-        // this.productService.getListProduct().subscribe((data) => {
-        //     this.listProduct = data;
-        //     console.log(this.listProduct);
-        // });
+        console.log(this.treeNodes);
     }
 
-    onProductClicked(dataItem: any) {
-        this.router.navigate(['/news', dataItem.id]);
+    // Use an arrow function to capture the 'this' execution context of the class.
+    public fetchChildren = (item: any): any[] => {
+        return item.ListChild;
+    };
+
+    public hasChildren = (item: any): boolean => {
+        return item.ListChild !== null;
+    };
+
+    test(item: any) {
+        console.log(item);
+        this.drawerView.toggle();
     }
 }
