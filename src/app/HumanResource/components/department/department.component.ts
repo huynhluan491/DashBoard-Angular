af1fe4afa28d05a8785b5845802ac44cf205830d
hnaul <huynhluan491@gmail.com>
2023-05-03 21:02:20 +0700

    CouponManagement-ADD-UI: thêm các components rời

--- src/app/HumanResource/components/department/department.component.ts
+++ src/app/HumanResource/components/department/department.component.ts
@@ -1,17 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { DrawerMode, DrawerPosition } from '@progress/kendo-angular-layout';
+import { LocationFormService } from '../../services/location-form.service';
 
 @Component({
     selector: 'app-department',
     templateUrl: './department.component.html',
     styleUrls: ['./department.component.scss'],
 })
-export class DepartmentComponent {
+export class DepartmentComponent implements OnInit {
     public expandMode: DrawerMode = 'overlay';
     public expanded = false;
     public position: DrawerPosition = 'end';
 
+    constructor(private locationFormService: LocationFormService) {
+
+    }
+
+    ngOnInit(): void {
+
+    }
+    
     handleOpenLocationForm(drawer: any) {
         drawer.toggle();
+        
     }
+
 }
