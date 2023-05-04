7e9af0cf815c69aeb655251a179a9a2f5c1a2bf0
hnaul <huynhluan491@gmail.com>
2023-05-04 16:06:38 +0700

    CouponManagement-ADD-UI: render filter-bar theo giá trị động

--- src/app/HumanResource/components/location/location.component.ts
+++ src/app/HumanResource/components/location/location.component.ts
@@ -1,28 +1,34 @@
 import { Component, OnInit } from '@angular/core';
 import { DrawerMode, DrawerPosition } from '@progress/kendo-angular-layout';
 import { LocationFormService } from '../../services/location-form.service';
-
+import { menuItems } from 'src/app/locationMenuItem';
 @Component({
     selector: 'app-location',
     templateUrl: './location.component.html',
     styleUrls: ['./location.component.scss'],
 })
-export class DepartmentComponent implements OnInit {
+export class LocationComponent implements OnInit {
+    filterBoxItems = [
+        {
+            addTypeName: 'THÊM ĐIỂM LÀM VIỆC',
+            iconUrl: 'DepartmentIcon.svg',
+        },
+        {
+            addTypeName: 'THÊM ĐIỂM LÀM VIỆC CON',
+            iconUrl: 'DepartmentIcon.svg',
+        },
+    ];
+
     public expandMode: DrawerMode = 'overlay';
     public expanded = false;
     public position: DrawerPosition = 'end';
+    locationMenuItems: any[] = menuItems;
 
-    constructor(private locationFormService: LocationFormService) {
+    constructor(private locationFormService: LocationFormService) {}
 
-    }
+    ngOnInit(): void {}
 
-    ngOnInit(): void {
-
-    }
-    
     handleOpenLocationForm(drawer: any) {
         drawer.toggle();
-        
     }
-
 }
